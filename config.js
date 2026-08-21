c2e23fbde98e416840e76ff380fdb104238e06aa
bot-792 <mee5ai.su07@gmail.com>
2026-08-21 05:40:02 +0100

    Update pairingNumber in config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212607244917;
+global.pairingNumber = 212709138520;
 global.owner = [
   ['212698498657', '𝗮𝗱𝗮𝗺.___.𝟵𝟴', true],
   ['', 'Owner 2', true],
